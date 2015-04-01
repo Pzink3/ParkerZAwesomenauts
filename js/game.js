96db18b12bfa20173a9855a13a3208b39ef9ceda
Parker Zink <parker.zink@gmail.com>
2015-04-01 13:49:26 -0700

    Added bases
    Added bases to the game

--- js/game.js
+++ js/game.js
@@ -13,7 +13,7 @@ var game = {
 	// Run on page load.
 	"onload" : function () {
 	// Initialize the video.
-	if (!me.video.init("screen",  me.video.CANVAS, 1067, 600, true, 'auto')) {
+	if (!me.video.init("screen",  me.video.CANVAS, 1067, 600, true, '1.0')) {
 		alert("Your browser does not support HTML5 canvas.");
 		return;
 	}
