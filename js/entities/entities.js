a99919fb8ea986c048e7145581b4bfb278dbd2ea
Parker Zink <parker.zink@gmail.com>
2015-04-01 22:42:39 -0700

    Added Gold & Experience Variables
    Added gold and experience variables for my game

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -17,6 +17,7 @@ game.PlayerEntity = me.Entity.extend({
     this.now = new Date().getTime();
     this.lastHit = this.now;
     this.dead = false;
+    this.attack = game.data.playerAttack;
     this.lastAttack = new Date().getTime();
     me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     this.renderable.addAnimation("idle", [78]); 
@@ -119,6 +120,10 @@ game.PlayerEntity = me.Entity.extend({
                  (((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
                  ){
              this.lastHit = this.now;
+             if(response.b.health <= game.data.playerAttack){
+                 game.data.gold += 1;
+                 console.log("Current gold: " + game.data.gold);
+             }
              response.b.loseHealth(game.data.playerAttack);
          }
      }
@@ -293,7 +298,7 @@ game.GameManager = Object.extend({
     init: function(x, y, settings){
         this.now = new Date().getTime();
         this.lastCreep = new Date().getTime();
-        
+        this.paused = false;
         this.alwaysUpdate = true;
     },
     
@@ -302,6 +307,10 @@ game.GameManager = Object.extend({
         if(game.data.player.dead){
             me.game.world.removeChild(game.data.player);
             me.state.current().resetPlayer(10, 0);
+        }
+         if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+            game.data.gold += 1;
+            console.log("Current gold: " + game.data.gold);
         }
         if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
             this.lastCreep = this.now;
