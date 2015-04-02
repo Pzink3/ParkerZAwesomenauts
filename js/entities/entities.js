be81732461d68ca284bf9726d29f23d53eae9982
Parker Zink <parker.zink@gmail.com>
2015-04-01 23:00:53 -0700

    Refactored init
    Refactored the init for my game

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,6 +1,22 @@
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings){
-        this._super(me.Entity, 'init', [x, y, {
+        this.setSuper();
+        this.setPlayerTimers();
+        this.setAttributes();
+    this.type = "PlayerEntity";
+        this.setFlags();
+    
+    
+    
+    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+    
+    this.addAnimation();
+   
+    this.renderable.setCurrentAnimation("idle");
+    },
+   
+    setSuper: function(){
+         this._super(me.Entity, 'init', [x, y, {
             image: "player",
                width: 64,
                height: 64,
@@ -10,22 +26,30 @@ game.PlayerEntity = me.Entity.extend({
                return(new me.Rect(0, 0, 64, 64)).toPolygon();
                }
         }]);
-    this.type = "PlayerEntity";
-    this.health = 20;
-    this.body.setVelocity(game.data.playerMoveSpeed, 20);
-    this.facing = "right";
+    },
+    
+    setPlayerTimers: function(){
     this.now = new Date().getTime();
-    this.lastHit = this.now;
-    this.dead = false;
-    this.attack = game.data.playerAttack;
+    this.lastHit = this.now;  
     this.lastAttack = new Date().getTime();
-    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+    },
+    
+    setAttributes: function(){
+    this.health = game.data.playerHealth;
+    this.body.setVelocity(game.data.playerMoveSpeed, 20);
+    this.attack = game.data.playerAttack;
+    },
+    
+    setFlags: function(){
+    this.facing = "right"; 
+    this.dead = false; 
+    },
+    
+    addAnimation: function(){
     this.renderable.addAnimation("idle", [78]); 
     this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
     this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
-    this.renderable.setCurrentAnimation("idle");
     },
-   
     update: function(delta){
         this.now = new Date().getTime();
         if (this.health <= 0){
